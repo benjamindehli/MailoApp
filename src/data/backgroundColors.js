24a07f9401f1bc95d97f2acf42aa4980df3dba92
Benjamin Dehli <superelg@gmail.com>
2024-12-15 17:12:15 +0100

    Add multiple backgrounds

---
+++ src/data/backgroundColors.js
@@ -0,0 +1,8 @@
+const backgroundColors = [
+    "linear-gradient(45deg, #007a75, #2d003f)",
+    "linear-gradient(45deg, #16343a, #366478)",
+    "linear-gradient(45deg, #250654, #8d0018)",
+    "linear-gradient(45deg, #0fe172, #246da3)",
+    "linear-gradient(45deg, #bf2218, #f4c702)",
+    "linear-gradient(45deg, #bf2218, #0c2665)"
+];
